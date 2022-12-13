712989e08386edb87007fb35cb19b029d839bd9e
MohamedAhmed-ceng <120516677+MohamedAhmed-ceng@users.noreply.github.com>
2022-12-13 23:55:22 +0100

    Add files via upload

---
+++ WSB-MohamedAhmed/TicTacToe_Game/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Threading;
+namespace Tic_Tac_Toe_Game
+
+{
+    class TicTacToe_Game
+
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int endGameCondidition = 0;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Player1: X and Player2: O");
+                Console.WriteLine("\n");
+                if (player % 2 == 0)
+                {
+                    Console.WriteLine("Player 2 turn");
+                }
+                else
+                {
+                    Console.WriteLine("Player 1 turn");
+                }
+                Console.WriteLine("\n");
+
+                Board();
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    if (arr[choice] != 'X' && arr[choice] != 'O')
+                    {
+                        if (player % 2 == 0)
+                        {
+                            arr[choice] = 'O';
+                            player++;
+                        }
+                        else
+                        {
+                            arr[choice] = 'X';
+                            player++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not allowed! The square {0} is already reserved for {1}", choice, arr[choice]);
+                        Console.WriteLine("\n");
+                    }
+                    endGameCondidition = CheckWin();
+                }
+                catch
+                {
+                    Console.WriteLine("Please insert a valid number between 1 and 9");
+                    Console.WriteLine("\n");
+                }
+                finally
+                {
+                    CheckWin();
+                }
+            }
+            while (endGameCondidition != 1 && endGameCondidition != -1);
+
+            Board();
+
+            if (endGameCondidition == 1)
+            {
+                Console.WriteLine("Player {0} is the winner!", (player % 2) + 1);
+            }
+            else
+            {
+                Console.WriteLine("Draw Match");
+            }
+            Console.ReadLine();
+        }
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+        private static int CheckWin()
+        {
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
